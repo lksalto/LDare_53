eb70cda60e773b8e6b9f2766bdb8ed47b60046d0
Portix <vitorsilva@estudante.ufscar.br>
2023-04-29 15:11:38 -0300

    Movimentação 3D

---
+++ LDare_53/Assets/Scripts/Player/PlayerMov3D.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMov3D : MonoBehaviour
+{
+    Vector2 input;
+    Rigidbody playerRb;
+    Vector3 moveDirection;
+
+    [SerializeField] float moveSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    private void FixedUpdate()
+    {
+        moveDirection = new Vector3(input.x, 0f, input.y).normalized;
+        //playerRb.AddForce(moveDirection * moveSpeed * 100f * Time.deltaTime, ForceMode.Force);
+        playerRb.velocity = moveDirection * moveSpeed;
+
+    }
+}
