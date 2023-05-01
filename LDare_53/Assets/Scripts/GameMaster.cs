7b0bd500910eb7593baeabf73291e1df8ae244b4
lksalto <saltolks@gmail.com>
2023-05-01 17:07:51 -0300

    UI
    ui problema

--- LDare_53/Assets/Scripts/GameMaster.cs
+++ LDare_53/Assets/Scripts/GameMaster.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
 public class GameMaster : MonoBehaviour
@@ -12,6 +13,11 @@ public class GameMaster : MonoBehaviour
     public Throwing playerBoxes;
     [SerializeField] TextMeshProUGUI scoreText;
     [SerializeField] TextMeshProUGUI timeText;
+    [SerializeField] GameObject UIDefault;
+    [SerializeField] GameObject endScreen;
+    [SerializeField] GameObject txtTime_txt;
+    [SerializeField] GameObject txtTime;
+    [SerializeField] GameObject txtDel;
     
     private void Start()
     {
@@ -24,11 +30,21 @@ public class GameMaster : MonoBehaviour
         timeText.text = Mathf.FloorToInt(time).ToString();
     }
 
-
     public void CountTotalBoxes(int number, int max)
     {
         scoreText.text = number.ToString() + "/" + max.ToString();
     }
 
+    public void FinishGame()
+    {
+        UIDefault.SetActive(false);
+        endScreen.SetActive(true);
+        txtTime_txt.GetComponent<TextMeshProUGUI>().text = txtTime.GetComponent<TextMeshProUGUI>().text;
+        txtDel.GetComponent<TextMeshProUGUI>().text = (playerBoxes.maxPackg - playerBoxes.pckgCount).ToString() + "/" + playerBoxes.maxPackg.ToString();
+    }
 
+    public void RestarLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
 }
