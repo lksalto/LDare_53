d39dcc8eb965327bc3b07476ed5ba26d9414666c
Ast <vitor.orsin@estudante.ufscar.br>
2023-05-01 19:25:17 -0300

    More dog stuff

--- LDare_53/Assets/Scripts/Enemy/DogAttack.cs
+++ LDare_53/Assets/Scripts/Enemy/DogAttack.cs
@@ -5,13 +5,23 @@ using UnityEngine;
 public class DogAttack : MonoBehaviour
 {
     public GameMaster gm;
+    public bool inCooldown = false;
 
     private void OnCollisionEnter(Collision other) {
         if(other.gameObject.tag == "Player"){
             Throwing player = other.gameObject.GetComponent<Throwing>();
-            if(player.pckgCount > 0)
+            if(player.pckgCount > 0 && !inCooldown){
+                StopAllCoroutines();
+                StartCoroutine(Cooldown());
                 player.ThrowBox(Random.insideUnitSphere.normalized, 10000f);
-            else gm.ResetLevel();
+            }
+            else gm.FinishGame();
         }
+    }
+
+    IEnumerator Cooldown(){
+        inCooldown = true;
+        yield return new WaitForSeconds(2f);
+        inCooldown = false;
     }
 }
